089be69b26c6c4a5cb4f0b03bf0fd22627eb209c
Taliya <arsemtalia@yandex.ru>
2019-12-10 16:28:49 +0300

    init

--- app/src/main/java/com/itis/template/MainActivity.kt
+++ app/src/main/java/com/itis/template/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.itis.template
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var adapter: SongsAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = SongsAdapter(SongRepository.getList()) { Song ->
+            navigateToSong(Song)
+        }
+        rv_songs.adapter = adapter
+    }
+
+    private fun navigateToSong(song: Song) {
+        startActivity(SongActivity.createIntent(
+                this, song.title, song.album, song.artist, song.cover, song.file))
+
+    }
+
+    companion object {
+        fun getDataSource(): List<Song> = arrayListOf(
+                Song("Let It Snow", "Frank Sinatra", "Christmas Songs", R.drawable.ph1, R.raw.let_it_snow)
+        )
     }
 }
+
